e8434874c8374c74a4532318da94cf4e96349b40
Mark Jason Galang <51401355+mgalang229@users.noreply.github.com>
2022-04-16 15:06:31 +0800

    Create Author.java

---
+++ Author.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Author {
+	
+	public static void main(String[] args) {	
+		FastScanner fs = new FastScanner();
+		PrintWriter out = new PrintWriter(System.out);
+		int T = 1;
+		T = fs.nextInt();
+		for(int tc = 0; tc < T; tc++) {
+			int n = fs.nextInt();
+			int[] p = new int[n];
+			for(int i = 0; i < n; i++) {
+				p[i] = fs.nextInt() - 1;
+			}
+			for(int i = 0; i < n; i++) {
+				int cnt = 0;
+				int index = i;
+				do {
+					cnt++;
+					index = p[index];
+				} while(index != i);
+				System.out.print(cnt + " ");
+			}
+			System.out.println();
+		}
+		out.close();
+	}
+	
+	static void sort(int[] a) {
+		ArrayList<Integer> arr = new ArrayList<>();
+		for(int x : a) {
+			arr.add(x);
+		}
+		Collections.sort(arr);
+		for(int i = 0; i < a.length; i++) {
+			a[i] = arr.get(i);
+		}
+	}
+		
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+		
+		String next() {
+			while(!st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+		int[] readArray(int n) {
+			int[] a = new int[n];
+			for(int i = 0; i < n; i++) {
+				a[i] = nextInt();
+			}
+			return a;
+		}
+		
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
